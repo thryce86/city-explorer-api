d1ec3368089eda8e6b80278191432cb4678f02f4
chad moore <thryce85@tamu.edu>
2022-04-21 10:29:37 -0500

    got weather api data in memory

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/xyz', (request, response) => {
   response.send('xyz');
 });
 
-app.get('/weather', (request, response) => {
+app.get('/weather',async  (request, response) => {
   // this will go to the terminal
   console.log('alive') ;
 //this will be what you use to pull the key  value down from the browser 
@@ -65,7 +65,7 @@ app.get('/weather', (request, response) => {
    console.log('map' , input.weather  ,input.valid_date) ;
    return new Forecast(input.weather , input.valid_date )
 //  return Forecast;
- }
+                       }
 
  
  );
@@ -83,9 +83,57 @@ app.get('/weather', (request, response) => {
 
 
 
+// I need to parse new input then somehow combine this to forecasts and send the entire thing 
+// I dont know how to make that request more specific by lat and lon 
+////////////////////////////////////////////////////////////////////////////////
+//lab08
+// let searchQuery_city = request.query.searchQuery;
+let searchQuery_lat = parseFloat(request.query.lat).toFixed(3);
+// let searchQuery_lon = parseFloat(request.query.lon).toFixed(3)   ;
+console.log('heya ' +  request.query.lon) ;
+let start_date = forecastArray[0].date;
+let end_date = forecastArray[2].date;
+let searchQuery_lon = parseFloat(request.query.lon).toFixed(3)   ;
+console.log("date_checker   =  "  +searchQuery_lat , searchQuery_lon ,start_date , end_date ) ;
 
-  response.send(forecastArray);
-  // console.log(request.query.name);
+// add start and end date
+//correct call I think #td need to do the call from 
+let weatherUrl = `https://api.weatherbit.io/v2.0/history/daily?&lat=${searchQuery_lat}&lon=${request.query.lon}&start_date=${start_date}&end_date=${end_date}&key=${process.env.WEATHER_API_KEY}` ;
+//test call
+//  let weatherUrl = `https://api.weatherbit.io/v2.0/history/daily?&lat=38.123&lon=-78.543&start_date=2022-04-17&end_date=2022-04-18&key=${process.env.WEATHER_API_KEY}` ;
+
+ // lat=48.858   lon=,2.3200 
+// let weatherUrl = `https://api.weatherbit.io/v2.0/history/daily?&lat=48.858&lon=2.320&start_date=2022-04-17&end_date=2022-04-18&key=${process.env.WEATHER_API_KEY}` ;
+
+console.log(weatherUrl) ;
+// console.log(weatherUrl) ; 
+
+let weatherDataTemp = await axios.get(weatherUrl);
+
+
+
+
+console.log('data from weaher: ' +  weatherDataTemp.data ) ;
+// console.log('data from weaher: ' )
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+  // response.send(forecastArray);
+  // console.log(forecastArray );
+  response.send(weatherDataTemp.data) ;
 });
 
 
@@ -113,6 +161,27 @@ class Forecast{
    this.description   = inWeather.description  ;
   }
 }
+
+
+// class OutputObj{
+//   constructor(windObj , forecastArray){
+
+//     // get the wind data
+
+//     // format the forecast data into everything 
+
+//     // return 
+//     // {
+//     //   "description": "Low of 17.1, high of 23.6 with broken clouds",
+//     //   "date": "2021-03-31"
+//     // }, 
+
+    
+
+
+
+//   }
+// }
 
 // this turns the server on to the port that you specifed in your .env file
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
